81c970a475b95c3d49137a7c7d6ebf055e69240c
lucasblazzi <lucasblazzi@hotmail.com>
2020-03-28 23:57:16 -0300

    Update - menu
    creating menu

--- certspotter.py
+++ certspotter.py
@@ -8,15 +8,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter domain: ')
-    serviceurl = 'https://api.certspotter.com/v1/issuances?domain=' + address + '&include_subdomains=true&expand=dns_names&expand=issuer&expand=cert'
-
-    if len(address) < 1: break
-
-    print()
-    print('Retrieving subdomains for:', address)
-    print()
+def dns_list(serviceurl):
     uh = urllib.request.urlopen(serviceurl, context=ctx)
     data = uh.read().decode()
 
@@ -29,3 +21,29 @@ while True:
         dns_name = js[count]['dns_names']
         for dns in dns_name:
             print(dns)
+
+def man_input(address):
+    while True:
+        address = input('Enter domain: ')
+        print()
+        print('Retrieving subdomains for:', address)
+        print()
+        serviceurl = 'https://api.certspotter.com/v1/issuances?domain=' + address + '&include_subdomains=true&expand=dns_names&expand=issuer&expand=cert'
+        if len(address) < 1: break
+        dns_list(serviceurl)
+        
+
+print("------------------------------------")
+print("-------CERTSPOTTER SUBDOMAINS-------")
+print("------------------------------------")
+print()
+print("1 - manual input")
+print("2 - file list input")
+
+menu = int(input("--->"))
+if menu == 1:
+    address = None
+    man_input(address)
+else:
+    quit()
+
