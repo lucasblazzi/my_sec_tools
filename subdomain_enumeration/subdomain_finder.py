e443deba29cf274c7cc09b5db1468fcab0cf31a2
lucasblazzi <lucasblazzi@hotmail.com>
2020-07-27 20:20:59 -0300

    file function
    added subdomain search using list of domains from a file

--- subdomain_enumeration/subdomain_finder.py
+++ subdomain_enumeration/subdomain_finder.py
@@ -10,10 +10,13 @@ ctx.verify_mode = ssl.CERT_NONE
 
 
 def dns_list(address):
+    print('\n[*] Searching for {} subdomains\n\n'.format(address))
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'
     services = [
-        {'api': 'crtsh', 'url': 'https://crt.sh/?q=%25.{}&output=json' .format(address)},
-        {'api': 'certspotter', 'url': 'https://api.certspotter.com/v1/issuances?domain={}&include_subdomains=true&expand=dns_names&expand=issuer&expand=cert' .format(address)}
+        {'api': 'crtsh', 'url': 'https://crt.sh/?q=%25.{}&output=json'.format(address)},
+        {'api': 'certspotter',
+         'url': 'https://api.certspotter.com/v1/issuances?domain={}&include_subdomains=true&expand=dns_names&expand=issuer&expand=cert'.format(
+             address)}
     ]
 
     subdomains = []
@@ -24,6 +27,7 @@ def dns_list(address):
             handler = urllib.request.urlopen(request)
         except urllib.error.HTTPError as e:  # manipulando erros HTTP
             print("CODE:", e.code, " -->", e.reason.upper())
+            print("Certspotter may have blocked this request, try again later or get an API Key")
             exit()
         except urllib.error.URLError:  # manipulando erros na URL
             print("Server down or incorrect domain")
@@ -54,19 +58,28 @@ def dns_list(address):
     print_subdomains(subdomains)
 
 
+def file_search(file):
+    file = open(file, 'r').read().split('\n')
+    for line in file:
+        dns_list(line)
+    print('\n\n\n')
+
+
 def print_subdomains(subdomains):
     subdomains = sorted(set(subdomains))
+    print('[*] {} subdomains found\n'.format(len(subdomains)))
     for dns in subdomains:
         print(dns)
 
 
-def menu():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find subdomains using certspotter and crtsh')
     parser.add_argument("-u", '--url', type=str,
-                        help="single target input")
+                        help="single target - simple input")
+    parser.add_argument("-f", '--file', type=str,
+                        help="multiple targets - input from file")
     args = parser.parse_args()
     if args.url:
         dns_list(args.url)
-
-
-menu()
+    if args.file:
+        file_search(args.file)
